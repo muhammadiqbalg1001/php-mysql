de7f4f58acf6cd42ac8aca4086cec0e04a80162f
Muhammad Iqbal Gobel <muhammadiqbal.g1001@gmail.com>
2026-05-12 06:43:08 +0700

    select query

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
 include("connection.php");
+
+$query = mysqli_query($connection, "SELECT * FROM pegawai");
+$result = mysqli_fetch_all($query, MYSQLI_ASSOC);
+
+echo "<pre>";
+print_r($result);
