84228c428007107bdbb4bffefb16800d8c0f0c0d
Muhammad Iqbal Gobel <muhammadiqbal.g1001@gmail.com>
2026-05-12 20:40:10 +0700

    show all data

--- index.php
+++ index.php
@@ -5,5 +5,43 @@ include("connection.php");
 $query = mysqli_query($connection, "SELECT * FROM pegawai");
 $result = mysqli_fetch_all($query, MYSQLI_ASSOC);
 
-echo "<pre>";
-print_r($result);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pegawai</title>
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>No.</th>
+                <th>Nama</th>
+                <th>Jenis Kelamin</th>
+                <th>Alamat</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($result as $index => $pegawai) : ?>
+                <tr>
+                    <td>
+                        <?php echo $index + 1?>
+                    </td>
+                    <td>
+                        <?php echo $pegawai["nama"]?>
+                    </td>
+                    <td>
+                        <?php echo $pegawai["jenis_kelamin"]?>
+                    </td>
+                    <td>
+                        <?php echo $pegawai["alamat"]?>
+                    </td>
+                </tr>
+            <?php endforeach ?>
+        </tbody>
+    </table>
+</body>
+</html>
